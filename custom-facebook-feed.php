6d84cc19d92c4d67ca444080d41c80e33e8eb715
smashballoon <smashballoon@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-11 22:06:42 +0000

    1.6.1
    git-svn-id: https://plugins.svn.wordpress.org/custom-facebook-feed/trunk@786669 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- custom-facebook-feed.php
+++ custom-facebook-feed.php
@@ -3,7 +3,7 @@
 Plugin Name: Custom Facebook Feed
 Plugin URI: http://smashballoon.com/custom-facebook-feed
 Description: Add a completely customizable Facebook feed to your WordPress site
-Version: 1.6.0
+Version: 1.6.1
 Author: Smash Balloon
 Author URI: http://smashballoon.com/
 License: GPLv2 or later
